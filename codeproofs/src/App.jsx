c8ec47d739a3b3749704d3afc2f4bc1658c5af6c
sagar <sagu7026@gmail.com>
2025-03-24 17:40:03 +0530

    setup

---
+++ codeproofs/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="bg-blue-500 text-white p-4">Hello, Tailwind!</div>
+
+  )
+}
+
+export default App
